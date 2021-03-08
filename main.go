7bbad7b7b67405bd96fd2ea59aebbf2f9dc92a8d
nicolas <nikolastesone@gmail.com>
2021-03-08 15:37:46 -0300

    database

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	handlers "github.com/avalith-net/skill-factory-go-carrerpath/controller"
+	db "github.com/avalith-net/skill-factory-go-carrerpath/database"
+)
+
 func main() {
 
+	if db.ConnectionCheck() == 0 {
+		log.Fatal("No connection to the database")
+		return
+	}
+	handlers.LaunchingServer()
 }
