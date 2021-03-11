bca7a053ded820ab68a7a734aa7c20b0f411a9c2
celestemasmut <39839619+celemasmut@users.noreply.github.com>
2021-03-10 21:15:42 -0300

    adding comments

--- models/userLogin.go
+++ models/userLogin.go
@@ -12,7 +12,6 @@ type User struct {
 	ID             primitive.ObjectID    `bson:"_id,omitempty" form:"id"`
 	Role           string                `bson:"role" form:"role,omitempty"`
 	Leader         string                `bson:"leader" form:"leader,omitempty"`
-	Admin          bool                  `bson:"admin" form:"admin,omitempty"`
 	Name           string                `bson:"name" form:"name,omitempty"`
 	Surname        string                `bson:"surname" form:"surname,omitempty"`
 	DateOfBirth    time.Time             `bson:"dateOfBirth" form:"dateOfBirth,omitempty"`
@@ -21,3 +20,5 @@ type User struct {
 	ProfilePhoto   *multipart.FileHeader `bson:"profilePhoto" form:"profilePhoto,omitempty"`
 	SeniorityLevel Seniority             `bson:"seniorityLevel" form:"seniorityLevel,omitempty"`
 }
+
+//each user will have a leader except de master leader.
