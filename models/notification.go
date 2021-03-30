43dd05b9091ca3d88928be4582d8d5e8b469d375
nicolas <nikolastesone@gmail.com>
2021-03-30 16:51:35 -0300

    sendNotificationToAdmin

--- models/notification.go
+++ models/notification.go
@@ -2,5 +2,5 @@ package models
 
 type Notification struct {
 	UserId  string `bson:"userid" json:"userid,omitempty"`
-	Message string `bson:"message" json:"message,omitempty`
+	Message string `bson:"message" json:"message,omitempty"`
 }
